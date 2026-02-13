c1f87ef5aa77ddaf085d2a86dbdee2520f514ff6
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-13 18:29:14 +0545

    Every things uptodated

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ecommerce/bindings/controller_bindings.dart';
+import 'package:ecommerce/routes/app_pages.dart';
+import 'package:ecommerce/view/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Mero Cart',
+      initialBinding: ControllerBindings(),
+      getPages: AppPages.routes,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: SplashView()
+    );
+  }
+}
