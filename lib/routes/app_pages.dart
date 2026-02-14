18d44bd56b9cd5d942963de7e35a1fdefdd42158
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-14 21:38:17 +0545

    Feat(Routes): App Pages Modified

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/routes/app_routes.dart';
 import 'package:ecommerce/view/login_view.dart';
+import 'package:ecommerce/view/register_view.dart';
 import 'package:ecommerce/view/splash_view.dart';
 import 'package:get/route_manager.dart';
 
@@ -7,5 +8,6 @@ class AppPages {
   static final routes = [
     GetPage(name: AppRoutes.splash, page: () => SplashView()),
     GetPage(name: AppRoutes.login, page: () => LoginView()),
+    GetPage(name: AppRoutes.register, page: () => RegisterView()),
   ];
 }
