cae420b25a7fb6ea3a687fce23dd28671400606a
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-15 18:01:56 +0545

    Feat(controller): Auth Controller Modified and Storage controller Added

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:ecommerce/controller/storage_controlle.dart';
+import 'package:ecommerce/model/login_model.dart';
 import 'package:ecommerce/model/register_model.dart';
 import 'package:ecommerce/routes/app_routes.dart';
 import 'package:ecommerce/services/auth_services.dart';
@@ -6,19 +8,35 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  RxBool isLoggedIn = false.obs;
   RxBool isLoading = false.obs;
+
+  //Registration Success response
   var registerResponce = RegisterModel(
     success: "false",
     token: null,
     message: null,
   ).obs;
 
+  //Error response
   var registerError = RegisterErrorModel(
     success: "false",
     errors: Errors(),
   ).obs;
 
+  //login response
+  var loginResponse = LoginModel(
+    success: false,
+    token: null,
+    message: null,
+  ).obs;
+
+  //login error response
+  var loginEror = LoginErrorModel(
+    success: "false",
+    errors: LoginErrors(email: []),
+  ).obs;
+
+  //text controller variables
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -27,11 +45,17 @@ class AuthController extends GetxController {
 
   //Splash Logic
   void checkAuth() {
+    final token = StorageController().getToken();
     Future.delayed(Duration(seconds: 5), () {
-      Get.offNamed(AppRoutes.login);
+      if (token == null) {
+        Get.offNamed(AppRoutes.login);
+      } else {
+        Get.offNamed(AppRoutes.home);
+      }
     });
   }
 
+  //User Registration Logic
   Future<void> registerUser() async {
     try {
       isLoading(true);
@@ -88,8 +112,6 @@ class AuthController extends GetxController {
           colorText: Colors.white,
           duration: Duration(seconds: 2),
         );
-        // Future.delayed(const Duration(seconds: 3), () {
-        // });
       }
     } on DioException catch (e) {
       if (e.response != null) {
@@ -100,9 +122,96 @@ class AuthController extends GetxController {
     }
   }
 
+  //login user logic
+  Future<void> loginUser() async {
+    try {
+      isLoading(true);
+      final response = await AuthServices.loginUser(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+
+      if (response.data["success"] == "false" ||
+          response.data["errors"] != null) {
+        final result = LoginErrorModel.fromJson(response.data);
+        loginEror.value = result;
+        isLoading(false);
+        if (!Get.isSnackbarOpen) {
+          Get.snackbar(
+            "",
+            "",
+            backgroundColor: Colors.red,
+            colorText: Colors.white,
+            titleText: const Text(
+              "Error",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            messageText: Text(
+              loginEror.value.errors!.email.first,
+              style: const TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          );
+        }
+      } else if (response.data["success"] == true) {
+        final result = LoginModel.fromJson(response.data);
+        loginResponse.value = result;
+        //save token
+        StorageController().saveToken(loginResponse.value.token!);
+        isLoading(false);
+        Get.offNamedUntil(AppRoutes.home, (route) => false);
+        Get.snackbar(
+          "Success",
+          "${loginResponse.value.message}",
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+          duration: Duration(seconds: 2),
+        );
+      } else {
+        final result = LoginModel.fromJson(response.data);
+        loginResponse.value = result;
+        isLoading(false);
+        Get.snackbar(
+          "Error",
+          "${loginResponse.value.message}",
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          duration: Duration(seconds: 2),
+        );
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  //logout logic
+  void logout() {
+    StorageController().deleteToken();
+    Get.offNamedUntil(AppRoutes.login, (route) => false);
+    Get.snackbar(
+      "Success",
+      "Logout Success",
+      backgroundColor: Colors.green,
+      colorText: Colors.white,
+      duration: Duration(seconds: 2),
+    );
+  }
+
   @override
   void onInit() {
     checkAuth();
     super.onInit();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
   }
 }
