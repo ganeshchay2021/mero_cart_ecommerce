07595434d0dd26307fd9f6c9dbceef1107b4ccd8
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-15 18:04:28 +0545

    Feat(Services): Auth Services Modified

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:ecommerce/utils/dio_connector.dart';
 
 class AuthServices {
+  //Method for Registration
   static Future<Response> registerUser({
     required String name,
     required String email,
@@ -9,11 +10,19 @@ class AuthServices {
   }) async {
     final response = await DioConnector.dio.post(
       "/register",
-      queryParameters: {
-        "name": name,
-        "email": email,
-        "password": password,
-      },
+      queryParameters: {"name": name, "email": email, "password": password},
+    );
+    return response;
+  }
+
+  //Method for login
+  static Future<Response> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    final response = await DioConnector.dio.post(
+      "/login",
+      queryParameters: {"email": email, "password": password},
     );
     return response;
   }
