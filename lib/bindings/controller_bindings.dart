754b0d3f2ea0e1c678b5e1b181cfb060a385d454
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-15 18:00:18 +0545

    Feat(Bindings): Bindings Modified

--- lib/bindings/controller_bindings.dart
+++ lib/bindings/controller_bindings.dart
@@ -1,9 +1,12 @@
 import 'package:ecommerce/controller/auth_controller.dart';
+import 'package:ecommerce/controller/storage_controlle.dart';
 import 'package:get/get.dart';
 
 class ControllerBindings extends Bindings{
   @override
   void dependencies() {
     Get.put<AuthController>(AuthController(), permanent: true);
+    Get.put<StorageController>(StorageController(), permanent: true);
+
   }
 }
