b58de1e898d94558ec9d963f453ac23f78287fa7
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-18 21:36:54 +0545

    Feat(Routes): App Pages Updated

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,7 +1,9 @@
+import 'package:ecommerce/model/product_model.dart';
 import 'package:ecommerce/routes/app_routes.dart';
 import 'package:ecommerce/view/botton_nav_bar.dart';
 import 'package:ecommerce/view/home_view.dart';
 import 'package:ecommerce/view/login_view.dart';
+import 'package:ecommerce/view/product_details_view.dart';
 import 'package:ecommerce/view/register_view.dart';
 import 'package:ecommerce/view/splash_view.dart';
 import 'package:get/route_manager.dart';
@@ -12,8 +14,13 @@ class AppPages {
     GetPage(name: AppRoutes.login, page: () => LoginView()),
     GetPage(name: AppRoutes.register, page: () => RegisterView()),
     GetPage(name: AppRoutes.home, page: () => HomeView()),
-    GetPage(name: AppRoutes.bottomNavBar, page: () =>  BottonNavBar()),
-
-
+    GetPage(name: AppRoutes.bottomNavBar, page: () => BottonNavBar()),
+    GetPage(
+      name: AppRoutes.product,
+      page: () {
+        final Product product = Get.arguments;
+        return ProductDetailsView(product: product);
+      },
+    ),
   ];
 }
