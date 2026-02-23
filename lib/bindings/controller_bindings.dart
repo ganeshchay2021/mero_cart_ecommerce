948dff5f9f497b28289ea4a6a7d400c12fc34b5a
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-23 10:43:25 +0545

    Feat(Bindings) : Controller Bindings Updated

--- lib/bindings/controller_bindings.dart
+++ lib/bindings/controller_bindings.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/controller/auth_controller.dart';
+import 'package:ecommerce/controller/cart_controller.dart';
 import 'package:ecommerce/controller/product_controller.dart';
 import 'package:ecommerce/controller/storage_controller.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,6 @@ class ControllerBindings extends Bindings{
     Get.put<AuthController>(AuthController(), permanent: false);
     Get.put<StorageController>(StorageController(), permanent: true);
     Get.put<ProductController>(ProductController(), permanent: true);
-
+    Get.put<CartController>(CartController(), permanent: true);
   }
 }
